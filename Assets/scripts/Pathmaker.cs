6b9e931ca7d27cf143c60e5aa607fb95f19c0427
Jess <zjess987@gmail.com>
2018-10-25 16:45:43 -0400

    25/10: main coding done, just need to finish art pass

--- Assets/scripts/Pathmaker.cs
+++ Assets/scripts/Pathmaker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // MAZE PROC GEN LAB
 // all students: complete steps 1-6, as listed in this file
@@ -15,24 +16,60 @@ public class Pathmaker : MonoBehaviour {
 // translate the pseudocode below
 
 //	DECLARE CLASS MEMBER VARIABLES:
-//	Declare a private integer called counter that starts at 0; 		// counter var will track how many floor tiles I've instantiated
-//	Declare a public Transform called floorPrefab, assign the prefab in inspector;
-//	Declare a public Transform called pathmakerSpherePrefab, assign the prefab in inspector; 		// you'll have to make a "pathmakerSphere" prefab later
+	private int counter = 0; 		// counter var will track how many floor tiles I've instantiated
+	public Transform floorPrefab;
+	public Transform pathmakerSpherePrefab;
 
+	public static int globalTileCount;
+	private int tileCounter;
+	private float rotateRightValue;
+
+	void Start()
+	{
+		tileCounter = Random.Range(50, 150);
+		rotateRightValue = Random.Range(0.05f, 0.15f);
+	}
+	
 
 	void Update () {
-//		If counter is less than 50, then:
-//			Generate a random number from 0.0f to 1.0f;
-//			If random number is less than 0.25f, then rotate myself 90 degrees;
-//				... Else if number is 0.25f-0.5f, then rotate myself -90 degrees;
-//				... Else if number is 0.99f-1.0f, then instantiate a pathmakerSpherePrefab clone at my current position;
-//			// end elseIf
+		
+		// destroy game object if globalTileCount > 500
+		if (globalTileCount > 500)
+		{
+			Destroy(gameObject);
+		}
 
-//			Instantiate a floorPrefab clone at current position;
-//			Move forward ("forward", as in, the direction I'm currently facing) by 5 units;
-//			Increment counter;
-//		Else:
-//			Destroy my game object; 		// self destruct if I've made enough tiles already
+		Debug.Log(globalTileCount);
+		
+		// instantiate sphere and floor if counter < 50
+		if (counter < tileCounter)
+		{
+			float randNum = Random.Range(0f, 1f);
+			if (randNum < rotateRightValue)
+			{
+				transform.Rotate(0f, 90f, 0f);
+			}
+			else if (randNum >= 0.3f && randNum <= 0.4f)
+			{
+				transform.Rotate(0f, -90f, 0f);
+			}
+			else if (randNum > 0.97f && randNum <= 1.0f)
+			{
+				Instantiate(pathmakerSpherePrefab, transform.position, Quaternion.Euler(0, 0, 0));
+			}
+			
+			
+			Instantiate(floorPrefab, transform.position, Quaternion.Euler(0, 0, 0));
+
+			transform.Translate(0f, 0f, 5f);
+
+			counter++;
+			globalTileCount++;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
 	}
 
 } // end of class scope
@@ -50,7 +87,8 @@ public class Pathmaker : MonoBehaviour {
 //	- put Pathmaker.cs on a sphere, configure all the prefabs in the Inspector, and test it to make sure it works
 //	STABILIZE: 
 //	- code it so that all the Pathmakers can only spawn a grand total of 500 tiles in the entire world; how would you do that?
-//	- (hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code? if there are already enough tiles, then maybe the Pathmaker could Destroy my game object
+//	- (hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code?
+//		if there are already enough tiles, then maybe the Pathmaker could Destroy my game object
 
 
 
@@ -67,18 +105,20 @@ public class Pathmaker : MonoBehaviour {
 // maybe randomize it even more?
 
 // - randomize 2 more variables in Pathmaker.cs for each different Pathmaker... you would do this in Start()
-// - maybe randomize each pathmaker's lifetime? maybe randomize the probability it will turn right? etc. if there's any number in your code, you can randomize it if you move it into a variable
+// - maybe randomize each pathmaker's lifetime? maybe randomize the probability it will turn right? etc.
+//   if there's any number in your code, you can randomize it if you move it into a variable
 
 
-
+       
 // STEP 6:  =====================================================================================
 // art pass, usability pass
 
 // - move the game camera to a position high in the world, and then point it down, so we can see your world get generated
 // - CHANGE THE DEFAULT UNITY COLORS, PLEASE, I'M BEGGING YOU
-// - add more detail to your original floorTile placeholder -- and let it randomly pick one of 3 different floorTile models, etc. so for example, it could randomly pick a "normal" floor tile, or a cactus, or a rock, or a skull
+// - add more detail to your original floorTile placeholder -- and let it randomly pick one of 3 different floorTile models, etc.
+// 	 so for example, it could randomly pick a "normal" floor tile, or a cactus, or a rock, or a skull
 //		- MODEL 3 DIFFERENT TILES IN MAYA! DON'T STOP USING MAYA OR YOU'LL FORGET IT ALL
-//		- add a simple in-game restart button; let us press [R] to reload the scene and see a new level generation
+// - add a simple in-game restart button; let us press [R] to reload the scene and see a new level generation
 // - with Text UI, name your proc generation system ("AwesomeGen", "RobertGen", etc.) and display Text UI that tells us we can press [R]
 
 
---
+++ Assets/scripts/reloadScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// put this on empty game object
+// will reload scene if you press R
+public class reloadScript : MonoBehaviour {
+
+	void Update () {
+		
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			Debug.Log("R to restart");
+			SceneManager.LoadScene("mainLabScene");
+
+			Pathmaker.globalTileCount = 0;
+		}
+	}
+}
